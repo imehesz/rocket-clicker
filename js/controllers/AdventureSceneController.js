aeaeb8ce365b9e4937a3f62688b5a7695a1fbbd1
Imre Mehesz <imehesz@web.com>
2017-12-09 22:23:12 -0500

    Put Booker on a ship

--- js/controllers/AdventureSceneController.js
+++ js/controllers/AdventureSceneController.js
@@ -4,7 +4,36 @@ class AdventureSceneController extends DefaultController {
   riotInit() {
     super.riotInit();
 
+    this.rs.distance = 0;
+    this.rs.speed = 0;
+    this.rs.addons = [];
+    this.rs.achievements = [];
+    const SLOWDOWN_CAP = 7;
+    let slowDownCap = SLOWDOWN_CAP;
+
     console.log("INIT FROM AdventureSceneController");
+
+    this.rs.speedUp = (e) => {
+      this.rs.speed = this.rs.speed + 1;
+      slowDownCap = SLOWDOWN_CAP;
+      e.preventDefault();
+    };
+
+    this.tickDistance = setInterval(() => {
+      this.rs.update({distance: Math.round(this.rs.distance + this.rs.speed)});
+      slowDownCap--;
+
+      if(slowDownCap <= 0) {
+        let tmpSpeed = Math.round(this.rs.speed) <= 0 ? 0 : this.rs.speed / 2;
+        this.rs.update({
+          speed: tmpSpeed
+        });
+      }
+    }, 1000);
+  }
+
+  onUnmount() {
+    clearInterval(this.tickDistance);
   }
 }
 
